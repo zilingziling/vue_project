648f5430a8d5e1ae51f47648d44e1fef2b3807e7
ziling <805217189@qq.com>
2019-12-25 15:03:09 +0800

    改价格

--- src/views/dashboard/components/LineChart.vue
+++ src/views/dashboard/components/LineChart.vue
@@ -65,7 +65,8 @@ export default {
         // axisPointer
         xAxis: {
           position: 'top',
-          data: xmonth,
+          // data: xmonth,
+          data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
           boundaryGap: false,
           axisTick: {
             show: false
