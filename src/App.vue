afa2b534399ad6d716bdf87345c38c5ffd28df47
ziling <805217189@qq.com>
2019-12-30 16:16:43 +0800

    修改了一下

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,16 @@
 <script>
 export default {
   name: 'App'
+  // mounted() {
+  //   window.addEventListener('message', this.receiveMessage, false)
+  // },
+  // methods: {
+  //   receiveMessage(event) {
+  //     const origin = event.origin
+  //     if (origin !== 'http://172.16.3.55:9528') return
+  //     alert(event.data)
+  //     // 事务处理代码
+  //   }
+  // }
 }
 </script>
